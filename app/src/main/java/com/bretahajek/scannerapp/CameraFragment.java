d05ab5adb7b65e74e5858282823e2384e0d86d87
Breta01 <breta.hajek@gmail.com>
2019-08-24 23:15:52 +0200

    Camera as fragment

--- app/src/main/java/com/bretahajek/scannerapp/CameraFragment.java
+++ app/src/main/java/com/bretahajek/scannerapp/CameraFragment.java
@@ -1,111 +1,207 @@
 package com.bretahajek.scannerapp;
 
+import android.app.ActionBar;
 import android.content.Context;
-import android.net.Uri;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Build;
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
-
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.util.Rational;
 import android.view.LayoutInflater;
+import android.view.TextureView;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.camera.core.CameraX;
+import androidx.camera.core.ImageCapture;
+import androidx.camera.core.ImageCaptureConfig;
+import androidx.camera.core.Preview;
+import androidx.camera.core.PreviewConfig;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.fragment.app.Fragment;
+
+import java.io.File;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link CameraFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link CameraFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class CameraFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private static int IMMERSIVE_FLAG_TIMEOUT = 300;
+    private OnFragmentInteractionListener callback;
+    private TextureView viewFinder;
+    private ConstraintLayout container;
+    private Runnable immersiveModeRunnable = new Runnable() {
+        @Override
+        public void run() {
+            ActionBar actionBar = getActivity().getActionBar();
+            if (actionBar != null) {
+                actionBar.hide();
+            }
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+            container.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
+                    | View.SYSTEM_UI_FLAG_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+        }
+    };
 
-    private OnFragmentInteractionListener mListener;
 
     public CameraFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment CameraFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static CameraFragment newInstance(String param1, String param2) {
-        CameraFragment fragment = new CameraFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        setRetainInstance(true);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        container.postDelayed(immersiveModeRunnable, IMMERSIVE_FLAG_TIMEOUT);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_camera, container, false);
-    }
-
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
     }
 
     @Override
     public void onAttach(@NonNull Context context) {
         super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
     }
 
     @Override
     public void onDetach() {
         super.onDetach();
-        mListener = null;
+        callback = null;
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
+    @Override
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        container = (ConstraintLayout) view;
+        container.post(immersiveModeRunnable);
+
+        viewFinder = (TextureView) container.getViewById(R.id.view_finder);
+        viewFinder.post(new Runnable() {
+            @Override
+            public void run() {
+                startCamera();
+            }
+        });
+    }
+
+    private void startCamera() {
+        DisplayMetrics metrics = new DisplayMetrics();
+        viewFinder.getDisplay().getRealMetrics(metrics);
+        Rational screenAspectRatio = new Rational(metrics.widthPixels, metrics.heightPixels);
+        PreviewConfig previewConfig = new PreviewConfig.Builder()
+                .setTargetAspectRatio(screenAspectRatio)
+                .build();
+        // @TODO: Add on layout change listener
+        Preview preview = new Preview(previewConfig);
+
+        preview.setOnPreviewOutputUpdateListener(
+                new Preview.OnPreviewOutputUpdateListener() {
+                    @Override
+                    public void onUpdated(Preview.PreviewOutput output) {
+                        ViewGroup parent = (ViewGroup) viewFinder.getParent();
+                        parent.removeView(viewFinder);
+                        parent.addView(viewFinder, 0);
+
+                        viewFinder.setSurfaceTexture(output.getSurfaceTexture());
+                    }
+                }
+        );
+
+        ImageCapture imageCapture = getImageCapture(screenAspectRatio);
+
+        CameraX.bindToLifecycle(this, preview, imageCapture);
+    }
+
+    private ImageCapture getImageCapture(Rational aspectRation) {
+        ImageCaptureConfig config = new ImageCaptureConfig.Builder()
+                .setTargetAspectRatio(aspectRation)
+                .setCaptureMode(ImageCapture.CaptureMode.MIN_LATENCY)
+                .build();
+        final ImageCapture imageCapture = new ImageCapture(config);
+
+        ImageButton captureButton = container.findViewById(R.id.capture_button);
+        captureButton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        File file = new File(
+                                getActivity().getExternalFilesDir(null),
+                                "test.jpg");
+                        Log.e(
+                                getString(R.string.app_name),
+                                "Saving to: " + file.getAbsolutePath());
+
+                        imageCapture.takePicture(file,
+                                new ImageCapture.OnImageSavedListener() {
+                                    @Override
+                                    public void onImageSaved(@NonNull File file) {
+                                        String msg = getString(R.string.image_save_success);
+                                        Toast.makeText(
+                                                getActivity().getBaseContext(),
+                                                msg,
+                                                Toast.LENGTH_SHORT).show();
+                                        Log.e(getString(R.string.app_name), msg);
+                                    }
+
+                                    @Override
+                                    public void onError(
+                                            @NonNull ImageCapture.UseCaseError useCaseError,
+                                            @NonNull String message,
+                                            Throwable cause) {
+                                        String msg = getString(R.string.image_save_failed);
+                                        Toast.makeText(
+                                                getActivity().getBaseContext(),
+                                                msg,
+                                                Toast.LENGTH_SHORT).show();
+                                        Log.e(getString(R.string.app_name), msg);
+                                        Log.e(getString(R.string.app_name), message);
+                                    }
+                                });
+
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                            // Display flash animation to indicate that photo was captured
+                            container.postDelayed(new Runnable() {
+                                @Override
+                                public void run() {
+                                    container.setForeground(new ColorDrawable(Color.BLACK));
+                                    container.postDelayed(new Runnable() {
+                                        @Override
+                                        public void run() {
+                                            container.setForeground(null);
+                                        }
+                                    }, 75);
+                                }
+                            }, 200);
+                        }
+                    }
+                }
+        );
+
+        return imageCapture;
+    }
+
+    public void setOnFragmentInteractionListener(OnFragmentInteractionListener callback) {
+        this.callback = callback;
+    }
+
+    /** Communicating with Other Fragments for more information.
      * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
      */
     public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
     }
 }
--- app/src/main/java/com/bretahajek/scannerapp/MainActivity.java
+++ app/src/main/java/com/bretahajek/scannerapp/MainActivity.java
@@ -1,37 +1,19 @@
 package com.bretahajek.scannerapp;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.camera.core.CameraX;
-import androidx.camera.core.ImageCapture;
-import androidx.camera.core.ImageCaptureConfig;
-import androidx.camera.core.ImageProxy;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-import androidx.camera.core.Preview;
-import androidx.camera.core.PreviewConfig;
-import androidx.lifecycle.LifecycleOwner;
-
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.graphics.Bitmap;
-import android.media.Image;
-import android.os.Bundle;
 import android.Manifest;
-import android.graphics.Matrix;
-import android.util.Log;
-import android.util.Rational;
-import android.view.Surface;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
 import android.view.TextureView;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ImageButton;
 import android.widget.Toast;
 
-import java.io.File;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements
+        CameraFragment.OnFragmentInteractionListener {
     private int REQUEST_CODE_PERMISSIONS = 10;
     private final String[] REQUIRED_PERMISSIONS = new String[] {
             Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE};
@@ -42,107 +24,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent myIntent = new Intent(this, CameraActivity.class);
-        startActivity(myIntent);
-
-
-        viewFinder = findViewById(R.id.view_finder);
-
-        if (allPermissionsGranted()) {
-            viewFinder.post(new Runnable() {
-                @Override
-                public void run() {
-                    startCamera();
-                }
-            });
-        } else {
+        if (!allPermissionsGranted()) {
             ActivityCompat.requestPermissions(
                     this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS);
         }
-    }
-
-    private ImageCapture getImageCapture(Rational aspectRation) {
-        ImageCaptureConfig config = new ImageCaptureConfig.Builder()
-                .setTargetAspectRatio(aspectRation)
-                .setCaptureMode(ImageCapture.CaptureMode.MIN_LATENCY)
-                .build();
-        final ImageCapture imageCapture = new ImageCapture(config);
-
-        ImageButton captureButton = findViewById(R.id.capture_button);
-        captureButton.setOnClickListener(
-            new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    File file = new File(getFilesDir(),  "test.jpg");
-                    imageCapture.takePicture(file,
-                        new ImageCapture.OnImageSavedListener() {
-                            @Override
-                            public void onImageSaved(@NonNull File file) {
-                                String msg = "Image saved successfully.";
-                                Toast.makeText(
-                                        getBaseContext(),
-                                        msg,
-                                        Toast.LENGTH_SHORT).show();
-                                Log.e("ScannerApp", msg);
-                            }
-                            @Override
-                            public void onError(
-                                    @NonNull ImageCapture.UseCaseError useCaseError,
-                                    @NonNull String message,
-                                    Throwable cause) {
-                                String msg = "Saving image failed. Please try again.";
-                                Toast.makeText(
-                                    getBaseContext(),
-                                    msg,
-                                    Toast.LENGTH_SHORT).show();
-                                Log.e("ScannerApp", msg);
-                                Log.e("ScannerApp", message);
-                            }
-                        });
-                }
-            }
-        );
-
-        return imageCapture;
-    }
-
-    private void startCamera() {
-        Rational screenAspectRatio = new Rational(viewFinder.getWidth(), viewFinder.getHeight());
-        PreviewConfig previewConfig = new PreviewConfig.Builder()
-                .setTargetAspectRatio(screenAspectRatio)
-                .build();
-        Preview preview = new Preview(previewConfig);
-
-        preview.setOnPreviewOutputUpdateListener(
-            new Preview.OnPreviewOutputUpdateListener() {
-                @Override
-                public void onUpdated(Preview.PreviewOutput output) {
-                    ViewGroup parent = (ViewGroup) viewFinder.getParent();
-                    parent.removeView(viewFinder);
-                    parent.addView(viewFinder, 0);
-
-                    viewFinder.setSurfaceTexture(output.getSurfaceTexture());
-                }
-            }
-        );
-
-        ImageCapture imageCapture = getImageCapture(screenAspectRatio);
-
-        CameraX.bindToLifecycle(this, preview, imageCapture);
     }
 
     @Override
     public void onRequestPermissionsResult(
             int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         if (requestCode == REQUEST_CODE_PERMISSIONS) {
-            if (allPermissionsGranted()) {
-                viewFinder.post(new Runnable() {
-                    @Override
-                    public void run() {
-                        startCamera();
-                    }
-                });
-            } else {
+            if (!allPermissionsGranted()) {
                 Toast.makeText(
                     this,
                     "Permissions not granted by the user.",
